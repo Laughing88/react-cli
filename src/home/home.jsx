97c2095fd5f7f098198588f6d8f0194f8f2ba42f
Laughing88 <Laughing_thg@163.com>
2022-12-16 20:55:54 +0800

    refs优化

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -4,26 +4,47 @@ import React from "react";
 class Home extends React.Component{
 
     clickValue = React.createRef();
-    DataBlur = React.createRef();
+    userName = React.createRef();
+    password = React.createRef();
 
     state = {
         isHot: false,
+        username: '',
+        password: '',
     }
 
     render() {
         const {name, age, sex} = this.props;
-        const {isHot} = this.state;
+        const {isHot  } = this.state;
         return(
             <div>
                 <h1 onClick={this.changeWeather}>今天天气很{isHot?'炎热':'凉爽'}</h1>
+
+                {/*props实现*/}
                 <ul>
                     <li>姓名：{name}</li>
                     <li>年龄：{age}</li>
                     <li>性别：{sex}</li>
                 </ul>
-                    <input type="text" placeholder="点击提示数据" ref={this.clickValue}/>&nbsp;
+
+                {/*refs推荐一种方式实现*/}
+                <input type="text" placeholder="点击提示数据" ref={this.clickValue}/>&nbsp;
                 <button onClick={this.showData}>点我提示左侧的数据</button>&nbsp;
-                <input type="text" placeholder="失去焦点提示数据" onBlur={this.showDataBlur} ref={this.DataBlur}/>
+                <input type="text" placeholder="失去焦点提示数据" onBlur={this.showDataBlur}/>
+
+                {/*非受控组件*/}
+                <form action="" onSubmit={this.handleSubmit}>
+                    <input type="text" name="username" ref={this.userName}/>
+                    <input type="password" name="password" ref={this.password}/>
+                    <button>登录</button>
+                </form>
+
+                {/*受控组件 推荐使用*/}
+                <form action="" onSubmit={this.handleControlledSubmit}>
+                    <input type="text" name="username" onChange={this.saveFromData('username')}/>
+                    <input type="password" name="password" onChange={this.saveFromData('password')}/>
+                    <button>登录</button>
+                </form>
             </div>
         )
     }
@@ -41,8 +62,31 @@ class Home extends React.Component{
     }
 
     //失去焦点弹窗显示
-    showDataBlur = ()=>{
-        alert(this.DataBlur.current.value);
+    showDataBlur = (event)=>{
+        alert(event.target.value);
+    }
+
+    //非受控组件提交事件
+    handleSubmit = (event)=>{
+        //阻止页面刷新
+        event.preventDefault();
+        alert(`你输入的用户名是：${this.userName.current.value},你输入的密码是：${this.password.current.value}`)
+    }
+
+    //保存表单数据到状态
+    saveFromData = (dataType)=>{
+        return (event)=>{
+            this.setState({
+                [dataType]: event.target.value,
+            })
+        }
+    }
+
+    //受控组件提交事件 推荐使用
+    handleControlledSubmit = (event)=>{
+        //阻止页面刷新
+        event.preventDefault();
+        alert(`你输入的用户名是：${this.state.username},你输入的密码是：${this.state.password}`)
     }
 }
 
